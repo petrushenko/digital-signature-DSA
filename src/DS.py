7248df422f6a37fbdbd69892620b741c14e16a6b
petrushenko <igor.petrushenko1@gmail.com>
2018-12-18 05:04:27 +0300

    some fixes

--- src/DS.py
+++ src/DS.py
@@ -1,16 +1,37 @@
 from math import sqrt
 from random import randint
-from hashlib import sha1
 from math import log
 
 #define
 
+def is_number(num):
+    '''
+    Проверяет является ли строка числом
+    '''      
+    #доступные символы
+    symb = [str(i) for i in range(10)]
+    if num.strip() == '':
+        return False
+    for dig in num:
+        if dig not in symb:
+            return False
+    return True       
+
 def bytes_needed(n):
+    '''
+    Размер числа в БАЙТАХ
+    '''
     if n == 0:
         return 1
     return int(log(n, 256)) + 1
 
 def dsa_check_file_sign(*, p, q, h, y, filename):
+    '''
+    Проверка ЭЦП на основе DSA
+    Возвращает код ошибки, либо:
+     -1 - подпись не прошла проверку
+      0 - подпись успешно прошла проверку
+    '''
     #check input values
     if (not (is_prime(q) and is_prime(p))):
         return 1
@@ -32,8 +53,16 @@ def dsa_check_file_sign(*, p, q, h, y, filename):
     my_hash = get_sha1(data)
     print(my_hash)
     my_hash = int(my_hash, 16)
-    r = int(split_data[1])
-    s = int(split_data[2])
+    if (len(split_data) == 3):
+        r = split_data[1]     
+        s = split_data[2]
+        if (is_number(r) and is_number(s)):
+            r = int(r)        
+            s = int(s)
+        else:
+            return 100
+    else:
+        return 100
 
     w = fast_pow(s, q - 2, q)
 
@@ -45,9 +74,9 @@ def dsa_check_file_sign(*, p, q, h, y, filename):
     v = ( ( fast_pow(g, u1, p) * fast_pow(y, u2, p) ) % p) % q
 
     if r == v:
-        return True
+        return 0
     else: 
-        return False
+        return -1
 
 def dsa_file_sign(*, p, q, h, k, x, filename):
     '''
@@ -65,7 +94,6 @@ def dsa_file_sign(*, p, q, h, k, x, filename):
         
         #Output open key y
         y = fast_pow(g, x, p)
-        print("y = {y}".format(y = y))
 
         f = open(filename, "r")
         data = f.read()
@@ -74,9 +102,8 @@ def dsa_file_sign(*, p, q, h, k, x, filename):
         f.close()
 
         my_hash = get_sha1(data)
-        print(my_hash)
+        hash_to_out = my_hash    
         my_hash = int(my_hash, 16)
-        print(bytes_needed(my_hash))
 
         r = (fast_pow(g, k, p) % q)
 
@@ -91,10 +118,10 @@ def dsa_file_sign(*, p, q, h, k, x, filename):
             f = open(filename, "w")
             f.write(data)
             f.close()
-            return (data, r, s)
+            return (data, r, s, y, hash_to_out)
         else:
-            return (-1, -1, -1)
-    return (err, err, err)  
+            return ("-Error", -1, -1, -1, -1)
+    return ("-Error", err, err, err, err)  
 
 def fast_pow(num, power, mod):
     '''
@@ -150,26 +177,10 @@ def is_prime(num, k = 50):
             return False # составное
     return True
 
-def isPrime(n, k=5): # miller-rabin
-    from random import randint
-    if n < 2: return False
-    for p in [2,3,5,7,11,13,17,19,23,29]:
-        if n % p == 0: return n == p
-    s, d = 0, n-1
-    while d % 2 == 0:
-        s, d = s+1, d/2
-    for i in range(k):
-        a = randint(2, n - 1)
-        x = fast_pow(a, d, n)
-        if x == 1 or x == n-1: continue
-        for r in range(1, s):
-            x = (x * x) % n
-            if x == 1: return False
-            if x == n-1: break
-        else: return False
-    return True
-
 def check_values(q, p, g, h, x, k):
+    '''
+    Проверка значений для получения ЭЦП на основе DSA
+    '''
     if (bytes_needed(q) < 20):
         return 1
     if (not is_prime(q)):
@@ -187,72 +198,77 @@ def check_values(q, p, g, h, x, k):
     return 0
 
 def get_sha1(data):
-       bytes = ""
+    '''
+    Реализация SHA-1 == стандартная библ.: from hashlib import sha1
 
-       h0 = 0x67452301
-       h1 = 0xEFCDAB89
-       h2 = 0x98BADCFE
-       h3 = 0x10325476
-       h4 = 0xC3D2E1F0
+    '''
+    pass
+    bytes = ""
 
-       for n in range(len(data)):
-           bytes+='{0:08b}'.format(ord(data[n]))
-       bits = bytes+"1"
-       pBits = bits
-       #pad until length equals 448 mod 512
-       while len(pBits)%512 != 448:
-           pBits+="0"
-       #append the original length
-       pBits+='{0:064b}'.format(len(bits)-1)
+    h0 = 0x67452301
+    h1 = 0xEFCDAB89
+    h2 = 0x98BADCFE
+    h3 = 0x10325476
+    h4 = 0xC3D2E1F0
 
-       def chunks(l, n):
-           return [l[i:i+n] for i in range(0, len(l), n)]
+    for n in range(len(data)):
+        bytes+='{0:08b}'.format(ord(data[n]))
+    bits = bytes+"1"
+    pBits = bits
+    #pad until length equals 448 mod 512
+    while len(pBits)%512 != 448:
+        pBits+="0"
+    #append the original length
+    pBits+='{0:064b}'.format(len(bits)-1)
 
-       def rol(n, b):
-           return ((n << b) | (n >> (32 - b))) & 0xffffffff
+    def chunks(l, n):
+        return [l[i:i+n] for i in range(0, len(l), n)]
 
-       for c in chunks(pBits, 512): 
-           words = chunks(c, 32)
-           w = [0]*80
-           for n in range(0, 16):
-               w[n] = int(words[n], 2)
-           for i in range(16, 80):
-               w[i] = rol((w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16]), 1)  
+    def rol(n, b):
+        return ((n << b) | (n >> (32 - b))) & 0xffffffff
 
-           a = h0
-           b = h1
-           c = h2
-           d = h3
-           e = h4
+    for c in chunks(pBits, 512): 
+        words = chunks(c, 32)
+        w = [0]*80
+        for n in range(0, 16):
+            w[n] = int(words[n], 2)
+        for i in range(16, 80):
+            w[i] = rol((w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16]), 1)  
 
-           #Main loop
-           for i in range(0, 80):
-               if 0 <= i <= 19:
-                   f = (b & c) | ((~b) & d)
-                   k = 0x5A827999
-               elif 20 <= i <= 39:
-                   f = b ^ c ^ d
-                   k = 0x6ED9EBA1
-               elif 40 <= i <= 59:
-                   f = (b & c) | (b & d) | (c & d) 
-                   k = 0x8F1BBCDC
-               elif 60 <= i <= 79:
-                   f = b ^ c ^ d
-                   k = 0xCA62C1D6
+        a = h0
+        b = h1
+        c = h2
+        d = h3
+        e = h4
 
-               temp = rol(a, 5) + f + e + k + w[i] & 0xffffffff
-               e = d
-               d = c
-               c = rol(b, 30)
-               b = a
-               a = temp
+        #Main loop
+        for i in range(0, 80):
+            if 0 <= i <= 19:
+                f = (b & c) | ((~b) & d)
+                k = 0x5A827999
+            elif 20 <= i <= 39:
+                f = b ^ c ^ d
+                k = 0x6ED9EBA1
+            elif 40 <= i <= 59:
+                f = (b & c) | (b & d) | (c & d) 
+                k = 0x8F1BBCDC
+            elif 60 <= i <= 79:
+                f = b ^ c ^ d
+                k = 0xCA62C1D6
 
-           h0 = h0 + a & 0xffffffff
-           h1 = h1 + b & 0xffffffff
-           h2 = h2 + c & 0xffffffff
-           h3 = h3 + d & 0xffffffff
-           h4 = h4 + e & 0xffffffff
+            temp = rol(a, 5) + f + e + k + w[i] & 0xffffffff
+            e = d
+            d = c
+            c = rol(b, 30)
+            b = a
+            a = temp
 
-       return '%08x%08x%08x%08x%08x' % (h0, h1, h2, h3, h4)
+        h0 = h0 + a & 0xffffffff
+        h1 = h1 + b & 0xffffffff
+        h2 = h2 + c & 0xffffffff
+        h3 = h3 + d & 0xffffffff
+        h4 = h4 + e & 0xffffffff
+
+    return '%08x%08x%08x%08x%08x' % (h0, h1, h2, h3, h4)
 
     
